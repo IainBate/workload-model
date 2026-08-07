e15c86f1ff6de110e12c9f463e14d7498022eef4
Iain Bate <iain.bate@york.ac.uk>
2026-08-07 20:31:56 +0100

    chore: auto-commit before tool use at 2026-08-07 20:31:56

--- scripts/workload_calculator.py
+++ scripts/workload_calculator.py
@@ -468,8 +468,9 @@ def _calculate_teaching_workload(module: ModuleData, teachers: List[str],
             )
 
             # Show individual lecturer breakdowns
-            # For new lecturers (or those with new content), show their calculation
-            for t in sorted(new_lecturers_practical + existing_with_new_content_practical):
+            # For new lecturers (or those with new content), show ONE generic line for all (same calculation)
+            if new_lecturers_practical or existing_with_new_content_practical:
+                t = sorted(new_lecturers_practical + existing_with_new_content_practical)[0]  # Just use first one
                 weekly_hrs = individual_practical_hours[t]
                 total_hrs = weekly_hrs * practical_week_count
                 practical_details.append(
